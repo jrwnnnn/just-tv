1e84c408683c5af4e70bb0db3263864267224cd5
Mark Jerwin <136870415+jrwnnnn@users.noreply.github.com>
2025-11-28 17:42:35 +0800

    Add TV guide XML and supporting script
    Added guide.xml containing TV schedule data and script/guide.js for guide functionality. Updated player.php to integrate guide features.

--- player.php
+++ player.php
@@ -6,6 +6,7 @@
     require_once 'functions/connect.php';
     require_once 'functions/useragent.php';
     
+    $id = $_GET['channel'];
     $channelData = query("SELECT * FROM channels WHERE id = ?", [$_GET['channel']] , "s");
     $sql = query("SELECT id FROM channels ORDER BY name ASC"); 
     $channels = array_column($sql, 'id');
@@ -29,9 +30,12 @@
     </div>
     <div class="flex justify-between items-center mt-4 mx-[10rem]">
         <img class="w-20 invert scale-x-[-1] hover:cursor-pointer" src="https://cdn-icons-png.flaticon.com/128/15948/15948774.png" onclick="history.replaceState(null, '', 'player.php?channel=<?= $previousChannel ?>'); location.reload();">
-        <div class="flex flex-col items-center space-y-2">
+        <div class="flex flex-col items-center space-y-1">
             <p class="text-center text-4xl font-bold"><?= $channelData['name']; ?></p>
-            <p class="font-mono text-center text-sm text-neutral-500">Status: <span id="playerStatus">Loading...</span></p>
+            <span id="currentShow">Loading...</span>
+            <div class="flex space-x-5 font-mono text-sm text-neutral-500">
+                <p>Upcoming: <span id="next"></span></p>
+            </div>
         </div>
         <img class="w-20 invert hover:cursor-pointer" src="https://cdn-icons-png.flaticon.com/128/15948/15948774.png" onclick="history.replaceState(null, '', 'player.php?channel=<?= $nextChannel ?>'); location.reload();">
     </div>    
@@ -41,8 +45,9 @@
         var player = document.getElementById('hlsPlayer');
         var streamUrl = '<?= $channelData['url']; ?>';
         var playerStatus = document.getElementById('playerStatus');
+        const channelId = "<?= $id ?>";
     </script>
     <script src="script/hls-handler.js"></script>
-
+    <script src="script/guide.js"></script>
 </body>
 </html>
